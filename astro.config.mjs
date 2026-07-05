5b318dad739dc6b49d949dc53721ec0b78360b9e
Kanav Nanda <kanavnanda965@gmail.com>
2026-07-05 06:36:11 +0530

    Set canonical site URL to kanavnanda.vercel.app

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://kanavnanda.dev',
+  site: 'https://kanavnanda.vercel.app',
 });
